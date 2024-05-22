2753d2b19cc19cecbf3a610bf31faed64079b82b
ch1cano <ch1canoo358@gmail.com>
2024-05-22 13:31:50 +0300

    hot fix scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import { CoinBlock } from "./components/CoinBlock/CoinBlock";
-// import { Home } from "./components/Home/Home";
+import { Home } from "./components/Home/Home";
 import { MakeTicket } from "./components/MakeTicket/MakeTicket";
 // import { MiniAppView } from "./components/MiniAppView/MiniAppView";
-// import { PlayNow } from "./components/PlayNow/PlayNow";
+import { PlayNow } from "./components/PlayNow/PlayNow";
 
 function App() {
 	return (
 		<>
 			<CoinBlock />
 			<MakeTicket />
-			{/* <PlayNow /> */}
+			<PlayNow />
 			{/* <MiniAppView /> */}
-			{/* <Home /> */}
+			<Home />
 		</>
 	);
 }
